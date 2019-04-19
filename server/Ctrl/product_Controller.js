ffb39b2396c53233d2609c4e25fa0ae653745234
Jeoffrey <jeoff.howe@gmail.com>
2019-04-19 11:16:45 -0600

    tested CRUD

---
+++ server/Ctrl/product_Controller.js
@@ -0,0 +1,27 @@
+module.exports = {
+    create(req, res) {
+        const db = req.app.get('db')
+        const { name, price, image_url } = req.body
+        db.create_product([name, price, image_url]).then(result => res.status(200).send(result))
+            .catch(err => console.log('err on create', err))
+    },
+
+    getAll(req, res) {
+        const db = req.app.get('db')
+        db.get_products().then(result => res.status(200).send(result))
+            .catch(err => console.log('err on getAll', err))
+    },
+    update(req,res) {
+        const db = req.app.get('db')
+        const {id} = req.params
+        const {name,price,image_url} = req.body
+        db.update_product([id,name,price,image_url]).then(result => res.status(200).send(result))
+        .catch(err=>console.log('err on update',err))
+    },
+    deleteProduct(req,res) {
+        const db = req.app.get('db')
+        const {id} = req.params
+        db.delete_product(id).then(result=> res.status(200).send(result))
+        .catch(err=>console.log('err on delete',err))
+    }
+}
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const massive = require('massive')
 const app = express();
+const productCtrl = require('./Ctrl/product_Controller')
 const {SERVER_PORT, CONNECTION_STRING} = process.env;
 app.use(express.json());
 
@@ -12,6 +13,11 @@ massive(CONNECTION_STRING).then(dbInstance=> {
 }).catch(err=>console.log('error on massive',err))
 
 
+app.get('/api/products', productCtrl.getAll)
+
+app.post('/api/products',productCtrl.create)
+app.put('/api/products/:id', productCtrl.update)
+app.delete('/api/products/:id', productCtrl.deleteProduct)
 
 
 
